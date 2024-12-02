24da07ba937ef71c300051996126bf7a6b366c8f
jmontoya05 <santiagomontoya997@gmail.com>
2024-12-02 17:03:07 -0500

    Creación de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+          <Route path="/dentist/:id" element={<div>Detail</div>} />
+          <Route path="/favs" element={<div>Favs</div>} />
+        </Routes>
+        <Footer />
       </div>
   );
 }
 
 export default App;
+
